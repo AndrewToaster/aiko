1e8d3d3dd4fbf3ac218b46e412cea6cbfbad2282
hamqtan <148271853+hamqtan@users.noreply.github.com>
2024-06-19 18:05:06 +0200

    Add files via upload

---
+++ Aiko/Scripts/Entities/EnemyTag.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+
+public class EnemyTag : MonoBehaviour, IEnemy
+{
+    public Sprite sprite;
+    public string enemyName;
+    public int maxHealth;
+
+    public Sprite GetBattleSprite()
+    {
+        return sprite;
+    }
+
+    public string GetBattleName()
+    {
+        return enemyName;
+    }
+
+    public int GetMaxHealth()
+    {
+        return maxHealth;
+    }
+}
---
+++ Aiko/Scripts/Entities/NPCData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NPCData", menuName = "SObjects/NPCData")]
+public class NPCData : ScriptableObject
+{
+    public string npcName;
+    public Sprite sprite;
+    public int health;
+}
